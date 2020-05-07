ff2400de294afbe2f7d95dcf3911f2560353f4ff
Anna Garczarek <ania.garczarek@gmail.com>
2020-05-07 20:25:07 +0200

    Create functions to add a new cards and columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from './Icon.js';
 
 import PropTypes from 'prop-types';
-//import {settings} from '../../data/dataStore.js';
+import {settings} from '../../data/dataStore';
+
 
 class Column extends React.Component {
-    
+
     static propTypes = {
       cards: PropTypes.array,
       title: PropTypes.node,
       icon: PropTypes.string,
+      addCard: PropTypes.func,
+    }
+    
+    static defaultProps = {
+      icon: settings.defaultColumnIcon,
     }
 
     render() {
-      const {title, icon, cards} = this.props;
+      const {title, icon, cards, addCard} = this.props;
       return (
         <section className={styles.component}> 
           <h3 className={styles.title}>
@@ -31,9 +37,9 @@ class Column extends React.Component {
             ))}
           </div>
           
-          {/* <div>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-          </div> */}
+          <div>
+            <Creator text={settings.cardCreatorText} action={addCard} />
+          </div>
 
         </section> 
         
