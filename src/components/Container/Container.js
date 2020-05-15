b0ea0e1a49fe6b596ac6edc7dfee3f5bc7b44862
Anna Garczarek <ania.garczarek@gmail.com>
2020-05-15 18:58:47 +0200

    Change file Container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,15 +2,7 @@ import React from 'react';
 import styles from './Container.scss';
 import PropTypes from 'prop-types';
 
-class Container extends React.Component {
-    
-  render() {
-    const {children} = this.props;
-    return (
-      <div className={styles.component}>{children}</div>
-    );
-  }
-}
+const Container = ({children}) => (<div className={styles.component}>{children}</div>);
 
 Container.propTypes = {
   children: PropTypes.node,
